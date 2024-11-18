083456074db6248fbd4675a7285c4e50bcc9237b
DanK <dan.kobylinski@gmail.com>
2024-11-18 16:03:11 +0100

    Add DbContext configuration

---
+++ Board/Entities/BoardContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Board.Entities
+{
+    public class BoardContext : DbContext
+    {
+        public BoardContext(DbContextOptions<BoardContext> options) : base(options) {}
+
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+
+}
--- Board/Program.cs
+++ Board/Program.cs
@@ -1,9 +1,15 @@
+using Board.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BoardContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BoardDbConnectionString")));
 
 var app = builder.Build();
 
@@ -13,5 +19,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.Run();
